d38dc133038058cb3658e5113630af0d5c1aa767
Peter Rose <peterroseq1@gmail.com>
2020-03-16 01:21:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,11 +8,11 @@ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
+   text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
